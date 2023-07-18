b91984dd224383e599afc846400d552e85bc1bc7
nati589 <natimok55@gmail.com>
2023-07-18 08:42:09 -0900

    Nav and footer layout

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -1,27 +1,22 @@
-import React from 'react'
-import WHO from "../assets/WHO.png"
-import UNDP from "../assets/UNDP.png"
-import Eth from "../assets/Ethiopian.png"
-
-
+import React from "react";
+import WHO from "../assets/WHO.png";
+import UNDP from "../assets/UNDP.png";
+import Eth from "../assets/Ethiopian.png";
 
 export default function Companies() {
-  const Companies=[WHO,UNDP,Eth,WHO,UNDP,Eth]
+  const Companies = [WHO, UNDP, Eth, WHO, UNDP, Eth];
   return (
-    <div className='container-fluid  text-center p-5 mb-3'>
-      <h2 className='mt-5 text-blue-500 p-2'>Trusted by</h2> 
+    <div className="container-fluid  text-center p-5 mb-3">
+      <h2 className="mt-5 text-blue-500 p-2">Trusted by</h2>
       <div className="container-fluid overflow-hidden row p-3">
-            
         {Companies.map((com, i) => {
           return (
-            <div className='col pt-2'>
-           <img width="120" height="100" src={com} alt="WHO"/>
-        </div>
-          )
+            <div className="col pt-2">
+              <img width="120" height="100" src={com} alt="WHO" />
+            </div>
+          );
         })}
-      
       </div>
-      
     </div>
-  )
+  );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,43 +1,48 @@
-import React from 'react'
-import { SlSocialTwitter } from "react-icons/sl"
-import {FiInstagram} from "react-icons/fi"
+import React from "react";
+import { SlSocialTwitter } from "react-icons/sl";
+import { FiInstagram } from "react-icons/fi";
 
 export default function Footer() {
   return (
-    <div className='bg-dark d-flex flex-column justify-content-center'>
-       <div className='d-flex flex-row p-5 justify-content-between' >
-      <div className='d-flex flex-row justify-content-evenly w-50'>
-        <div className='d-flex flex-column p-3'>
-          <h3 className='text-light'>Learn more</h3>
-          <a  className="text-secondary text-decoration-none" href="#">About HBGB</a>
-          <a  className="text-secondary text-decoration-none" href="#">Services</a>
-          <a className="text-secondary text-decoration-none" href="#">Contact Us</a>
+    <div className="bg-dark d-flex flex-column justify-content-center">
+      <div className="d-flex flex-row p-5 justify-content-between">
+        <div className="d-flex flex-row justify-content-evenly w-50">
+          <div className="d-flex flex-column p-3">
+            <h3 className="text-light">Learn more</h3>
+            <a className="text-secondary text-decoration-none" href="#">
+              About HBGB
+            </a>
+            <a className="text-secondary text-decoration-none" href="#">
+              Services
+            </a>
+            <a className="text-secondary text-decoration-none" href="#">
+              Contact Us
+            </a>
+          </div>
+          <div className="p-3 w-50 ">
+            <h3 className="text-light">Contact</h3>
+            <p className="text-secondary">
+              Aberus Building,5th floor in front of Dembel Building.
+            </p>
+            <p className="text-secondary">supporthbgbtrading@gmail.com</p>
+            <p className="text-secondary">+251-920-735-895</p>
+          </div>
         </div>
-        <div className='p-3 w-50 '>
-          <h3 className='text-light'>Contact</h3>
-          <p className="text-secondary">
-            Aberus Building,5th floor in front of Dembel Building.
-          </p>
-          <p className="text-secondary">
-            supporthbgbtrading@gmail.com
-          </p>
-          <p className="text-secondary">
-            +251-920-735-895
-          </p>
-        </div>
+        <div
+          className="bg-white w-40 h-40 mx-5"
+          style={{
+            width: "300px",
+            height: "300px",
+            backgroundColor: "white",
+          }}></div>
       </div>
-      <div className='bg-white w-40 h-40 mx-5' style={{ width: '300px',height:'300px', backgroundColor:"white"}}>
-
-      </div>
-      </div>
-      <div className='container d-flex flex-row justify-content-between  border-top border-light-subtle mx-auto p-4'>
-        <p className='text-secondary'> &copy; 2023 HBGB,plc...</p>
-        <div className='d-flex flex-row text-white justify-content-between p-2'>
-          <FiInstagram  className='mx-3'/>
+      <div className="container d-flex flex-row justify-content-between  border-top border-light-subtle mx-auto p-4">
+        <p className="text-secondary"> &copy; 2023 HBGB,plc...</p>
+        <div className="d-flex flex-row text-white justify-content-between p-2">
+          <FiInstagram className="mx-3" />
           <SlSocialTwitter />
         </div>
       </div>
     </div>
-   
-  )
+  );
 }
